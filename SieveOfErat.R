e11ffc4bd6e3a6126df914ffbc28e2a091a7aebe
Roberto Guzman Jr <rg255g@gmail.com>
2015-05-23 14:52:58 -0400

    Create SieveOfErat.R

---
+++ SieveOfErat.R
@@ -0,0 +1,15 @@
++SieveOfErat <- function(N)
++{
++    if(N == 1) {return(1)}
++    vec = c(2:N)
++    primes = c()
++    upper = ceiling(sqrt(N))
++    current = 1
++    while(vec[1] <= upper)
++    {
++        primes = c(primes,vec[1])
++        vec = vec[vec %% vec[1] != 0]
++    }
++    primes = c(primes,vec)
++    return(primes)
++}
