d8cc1d1fb04fcfb7045fd4fc9078c79a87a7028f
Roberto Guzman Jr <rg255g@gmail.com>
2015-06-24 11:40:13 -0400

    Create Problem_65.cs

---
+++ 61-70/Problem_65.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE65
+{
+    class Program
+    {
+        public static Tuple<BigInteger, BigInteger> ComputeNthConvergent(int[] coefficientList)
+        {
+            BigInteger h_0 = 0;
+            BigInteger h_1 = 1;
+            BigInteger k_0 = 1;
+            BigInteger k_1 = 0;
+
+            for (var i = 0; i < coefficientList.Length; i++)
+            {
+                BigInteger h_temp = h_1;
+                BigInteger k_temp = k_1;
+                h_1 = coefficientList[i] * h_1 + h_0;
+                k_1 = coefficientList[i] * k_1 + k_0;
+                h_0 = h_temp;
+                k_0 = k_temp;
+            }
+
+            return new Tuple<BigInteger, BigInteger>(h_1, k_1);
+        }
+
+        static void Main(string[] args)
+        {
+            var coefficients = new int[100];
+            coefficients[0] = 2;
+            coefficients[1] = 1;
+            for (var j = 2; j < coefficients.Length; j++)
+            {
+                coefficients[j] = (j + 1) % 3 == 0 ? 2 * ((j + 1) / 3) : 1;
+            }
+
+            var result = ComputeNthConvergent(coefficients);
+            var numerator = result.Item1;
+            BigInteger sum = 0;
+            while (numerator > 0)
+            {
+                sum += numerator % 10;
+                numerator /= 10;
+            }
+
+            Console.WriteLine(sum);
+            Console.WriteLine("Done");
+            Console.ReadLine();
+        }
+    }
+}
